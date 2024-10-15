bb8eeaa803c6332be0f6e194bd8f4865cdc41b39
josue130 <josuelopez67i90@gmail.com>
2024-10-14 19:55:42 -0600

    Add ReportController

---
+++ WorkoutAPI/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Workout.Application.Common.Dto;
+using Workout.Application.Services.Interface;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WorkoutAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ReportController : ControllerBase
+    {
+        private readonly IWorkoutPlanService _workoutPlanService;
+        private readonly ResponseDto _response;
+        public ReportController(IWorkoutPlanService workoutPlanService)
+        {
+            _workoutPlanService = workoutPlanService;
+            _response = new();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            IEnumerable<WorkoutPlanResponseDto> data = await _workoutPlanService.GenerateReport(User);
+            _response.result = data;
+            return Ok(_response);
+        }
+    }
+}
