3f477b8a7795160a55c7e98c4120903a87e8a3be
josue130 <josuelopez67i90@gmail.com>
2024-10-14 20:04:36 -0600

    Update WorkoutCommentsDto

--- Workout.Application/Common/Dto/WorkoutCommentsDto.cs
+++ Workout.Application/Common/Dto/WorkoutCommentsDto.cs
@@ -14,6 +14,6 @@ namespace Workout.Application.Common.Dto
         public Guid? Id { get; set; }
         public Guid WorkoutId { get; set; }
         public string Comment { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime? Date { get; set; }
     }
 }
