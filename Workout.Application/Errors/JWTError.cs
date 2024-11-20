1dda9705730a96bb1afcbf75a901382a735ae30e
josue130 <josuelopez67i90@gmail.com>
2024-11-20 16:57:32 -0600

    feat(application): add jwt error

---
+++ Workout.Application/Errors/JWTError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workout.Application.Common.Result;
+
+namespace Workout.Application.Errors
+{
+    public static class JWTError
+    {
+        public static readonly Error JwtTokenInvalid = new Error(
+            "Token", "Jwt token invalid");
+
+    }
+
+}
